03a874bed7548b013d3a1e422498f5a0f1a2c7b0
Samudrala Jeevana Lakshmi <113153018+samudralajeevanalakshmi@users.noreply.github.com>
2024-06-06 14:52:36 +0530

    Update jobseeker.service.ts

--- src/app/Service/jobseeker.service.ts
+++ src/app/Service/jobseeker.service.ts
@@ -8,7 +8,7 @@ import { Application } from '../Model/application';
   providedIn: 'root'
 })
 export class JobseekerService {
-  base_url = "http://4.255.45.27:8082/";
+  base_url = "http://52.179.239.186:8082/";
   constructor(private http: HttpClient) {   }
 
   public getJobsByUserId(id:Number):Observable <any>{
